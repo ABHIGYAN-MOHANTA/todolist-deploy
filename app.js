e1bc94831c19841a6a26d2ac4055374c3981e54f
ABHIGYAN-MOHANTA <underthunder02@gmail.com>
2022-09-22 12:48:02 +0530

    I

--- app.js
+++ app.js
@@ -128,6 +128,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT||3000, () => {
+app.listen(process.env.PORT||3000, function(){
   console.log("Server started Successfully");
 });
